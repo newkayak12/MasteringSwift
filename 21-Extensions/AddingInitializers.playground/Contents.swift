38de1766be7932ebb4522a450ccb35f0329bbf75
newkayak12 <newkayak12@gmail.com>
2022-10-23 17:28:37 +0900

    masteringSwift

--- 21-Extensions/AddingInitializers.playground/Contents.swift
+++ 21-Extensions/AddingInitializers.playground/Contents.swift
@@ -1,24 +1,3 @@
-//
-//  Copyright (c) 2018 KxCoding <kky0317@gmail.com>
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
 import UIKit
 
 /*:
@@ -27,13 +6,46 @@ import UIKit
 
 
 
+extension Date {
+    init?(year: Int, month: Int, day: Int) {
+        let cal = Calendar.current
+        var comp = DateComponents()
+        comp.year = year
+        comp.month = month
+        comp.day = day
+        
+        guard let date = cal.date(from: comp) else {
+            return nil
+        }
+        
+        self = date
+    }
+}
+
+let d = Date(year: 2022, month: 10, day: 23)
+let d2 = Date(year: -1, month: 10, day: 11)
 
 
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int){
+        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1.0)
+    }
+}
+
+UIColor(red: 0, green: 0, blue: 255)
 
 
+struct Size {
+    var width = 0.0
+    var height = 0.0
+}
 
-
-
-
-
+extension Size {
+    init(value: Double){
+        width = value
+        height = value
+    }
+}
+Size()
+Size(width: 11, height: 34)
 
